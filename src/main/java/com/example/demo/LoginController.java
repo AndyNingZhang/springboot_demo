335237719e5e275bb68a77d408604f335a1cc4a7
e <348959636@qq.com>
2019-07-21 16:05:17 +0800

    提交GitHup仓库

---
+++ src/main/java/com/example/demo/LoginController.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class LoginController {
+
+    // 创建线程安全的Map
+    static Map<Integer, User> users = Collections.synchronizedMap(new HashMap<Integer, User>());
+
+    /**
+     * 根据ID查询用户
+     * @param id
+     * @return
+     */
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer", paramType = "path")
+    @RequestMapping(value = "user/{id}", method = RequestMethod.GET)
+    public ResponseEntity<JsonResult> getUserById (@PathVariable(value = "id") Integer id){
+        JsonResult r = new JsonResult();
+        try {
+            User user = users.get(id);
+            r.setResult(user);
+            r.setStatus("ok，访问成功！");
+            System.out.println("访问成功。。。。。。。。");
+        } catch (Exception e) {
+            r.setResult(e.getClass().getName() + ":" + e.getMessage());
+            r.setStatus("error");
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok(r);
+    }
+
+}
+
